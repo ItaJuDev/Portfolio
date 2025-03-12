4c688217367074d8d7d2d7641e0911dff46714bb
ZenithXX <vitit.c@kkumail.com>
2025-03-12 18:27:41 +0700

    test vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false, // Disable server-side rendering (for static deployment)
+  ssr: true,
   nitro: {
-    preset: "static", // Enable static site generation
+    preset: "vercel",
   },
   compatibilityDate: "2024-11-01",
   modules: ["@nuxtjs/tailwindcss"],
